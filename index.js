7ca372c0bea2f77208fa798df40f3c48a749a93c
mine-martin <wangondumn@gmail.com>
2022-04-05 00:06:08 +0300

    Added map array method

---
+++ index.js
@@ -0,0 +1,12 @@
+//javascript array functions/methods
+//map function
+
+const nums = [1, 2, 3, 4, 5, 6];
+
+//map through the array and return the square of each element
+let newArr = nums.map((num, index) => {
+  //return each array element squared
+  return num * num;
+});
+
+// console.log(newArr);
