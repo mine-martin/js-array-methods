d9e799d746907939e217f36bc240bf9820dbc20a
mine-martin <wangondumn@gmail.com>
2022-04-04 23:58:29 +0300

    Added filter method

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// //javascript array functions/methods
-// //map function
-
-// const nums = [1, 2, 3, 4, 5, 6];
-
-// //map through the array and return the square of each element
-// let newArr = nums.map((num, index) => {
-//   //return each array element squared
-//   return num * num;
-// });
-
-// // console.log(newArr);
-
-// //forEach function
-// //forEach is a method that takes a function as an argument
-
-// const fruits = ['apple', 'banana', 'orange', 'pineapple', 'pear'];
-
-// let newFruits = fruits.forEach((fruit, index) => {
-//   // console.log();
-//   console.log('the index of ' + fruit + ' is ' + index);
-// });
-
-//filter array method
-//Array -> [filter] -> filtered value
-
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 let result = nums.filter((num) => {
